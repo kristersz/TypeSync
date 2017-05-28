c0d2ebeb96a07a6b4e52ed77ed6d7855b34d52fb
Kristers <kristers.zimecs@outlook.com>
2017-05-29 01:14:37 +0300

    Fix TsGenerator Http calls

--- src/TypeSync.Output/Generators/TsGenerator.cs
+++ src/TypeSync.Output/Generators/TsGenerator.cs
@@ -23,14 +23,14 @@ namespace TypeSync.Output.Generators
         public TsGenerator()
         {
             _client = new HttpClient();
+
+            _client.BaseAddress = new Uri("http://localhost:8080");
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
         }
 
         private string CallGenerator(string path, HttpContent content)
         {
-            _client.BaseAddress = new Uri("http://localhost:8080");
-            _client.DefaultRequestHeaders.Accept.Clear();
-            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
             var response = _client.PostAsync(path, content).Result;
 
             if (response.IsSuccessStatusCode)
